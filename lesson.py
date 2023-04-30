eb07ee5e9f8f5348184a58b13c2c227eb40e8f20
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-01 00:35:15 +0300

    correct PEP8 errors

--- lesson.py
+++ lesson.py
@@ -2,10 +2,10 @@ import sqlite3
 
 """Подключение к существующей базе данных"""
 
-db = sqlite3.connect(r'/Users/vitalybesedin/Documents/SQLite/qa_testing.db') # подключение к базе данных
+db = sqlite3.connect(r'/Users/vitalybesedin/Documents/SQLite/qa_testing.db')  # подключение к базе данных
 print("Подключились к базе данных")
-cur = db.cursor() # Переменная для управления базой данных
-cur.execute("""SELECT * FROM Students;""") # Запрос для получения содержимого таблицы Students
-result = cur.fetchall() # Результат запроса
+cur = db.cursor()  # Переменная для управления базой данных
+cur.execute("""SELECT * FROM Students;""")  # Запрос для получения содержимого таблицы Students
+result = cur.fetchall()  # Результат запроса
 print(result)
 print(type(result))
